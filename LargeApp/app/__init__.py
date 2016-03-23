6fafcd690e7de11cb05a4e1b0e1ef64d72b03e06
Manuel <m_koes17@uni-muenster.de>
2016-03-23 21:08:14 +0100

    added flask_restful api initialisation

--- LargeApp/app/__init__.py
+++ LargeApp/app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template
+from flask_restful import Api
 
 from app.VizpyML.controllers import Vizpymzml
 # from app.Module_Two.controllers import WHATEVER
 
 app = Flask(__name__)
+api = Api(app)
 app.config.from_object('config')
 
 
