c3a1797f110e60b89d1fff658857dfb190cbf880
Manuel <m_koes17@uni-muenster.de>
2016-03-23 21:17:44 +0100

    initialize Api in blueprint

--- LargeApp/app/VizpyML/controllers.py
+++ LargeApp/app/VizpyML/controllers.py
@@ -1,7 +1,10 @@
 from flask import Blueprint, request, render_template, \
                   flash, g, session, redirect, url_for
+from flask_restful import Api
 
 Vizpymzml = Blueprint("Vizpymzml", __name__, url_prefix='/VizML')
+api = Api(Vizpymzml)
+
 
 @Vizpymzml.route('/')
 def index():
--- LargeApp/app/__init__.py
+++ LargeApp/app/__init__.py
@@ -1,11 +1,9 @@
 from flask import Flask, render_template
-from flask_restful import Api
 
 from app.VizpyML.controllers import Vizpymzml
 # from app.Module_Two.controllers import WHATEVER
 
 app = Flask(__name__)
-api = Api(app)
 app.config.from_object('config')
 
 
