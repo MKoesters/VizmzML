9b722bea4b0b945c7f47f343874be7611179f37d
Manuel <m_koes17@uni-muenster.de>
2016-03-23 19:49:59 +0100

    updated package

--- LargeApp/app/VizpyML/controllers.py
+++ LargeApp/app/VizpyML/controllers.py
@@ -1,6 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, request, render_template, \
+                  flash, g, session, redirect, url_for
 
-Vizpymzml = Blueprint("Vizpymzml", __name__)
+Vizpymzml = Blueprint("Vizpymzml", __name__, url_prefix='/VizML')
 
 @main.route('/')
 def index():
--- LargeApp/app/__init__.py
+++ LargeApp/app/__init__.py
@@ -1,10 +1,14 @@
-from flask import Flask
-from app.Vizpymzml.controller import Vizpymzml
-from app.Module_Two.controller import WHATEVER
+from flask import Flask, render_template
+
+from app.VizpyML.controllers import Vizpymzml
+from app.Module_Two.controllers import WHATEVER
 
 app = Flask(__name__)
+app.config.from_object('config')
+
+
 
 app.register_blueprint(Vizpymzml, url_prefix='/')
-app.register_blueprint(WHATEVER, url_prefix='/whatever')
+#app.register_blueprint(WHATEVER, url_prefix='/whatever')
 
 
